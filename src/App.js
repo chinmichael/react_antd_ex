eca67465fa061d538bc7b79c37924e8654d9afba
chinmichael <chinmichael@naver.com>
2021-06-23 23:53:43 +0900

    ANTD 기본 공부1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { useState } from 'react';
+
+import { Row, Col, Layout, Button } from 'antd';
+import { } from '@ant-design/icons';
+
+const { Content, Sider } = Layout;
+
 
 function App() {
+  let [toggle, toggleSet] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <Layout style={{ minHeight: '100vh' }}>
+        {/* <Sider width={400} breakpoint="md" collapsedWidth={0}> */}
+        {/* <Sider width={400} collapsible collapsed={toggle}> */}
+        <Sider collapsed={false} onCollapse={false}>
+
+        </Sider>
+        <Layout>
+          <Content>
+            <Row justify="center">
+              <Col span={24} sm={6} className="test-col">
+                Col1
+              </Col>
+              <Col span={24} sm={6} className="test-col">
+                Col2
+              </Col>
+              <Col span={24} sm={6} className="test-col">
+                Col3
+              </Col>
+            </Row>
+            <Button type="danger" onClick={() => { toggleSet(!toggle) }}>button</Button>
+          </Content>
+        </Layout>
+      </Layout>
+    </div >
   );
 }
 
